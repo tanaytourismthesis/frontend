ce8c8b9e4e60fe87f2b4596581c8f0ced8346510
tanaytourismthesis <tanaytourismthesis@gmail.com>
2018-09-09 23:14:58 +0800

    updated page

---
+++ application/modules/fc/views/fc.php
@@ -0,0 +1,54 @@
+<div class="container-fluid festival">
+  <div class="row section-title">
+    <img src="assets/images/hand.gif">
+      <span>
+        <span class="text-f">F</span>
+        <span class="text-e">E</span>
+        <span class="text-s">S</span>
+        <span class="text-t">T</span>
+        <span class="text-i">I</span>
+        <span class="text-v">V</span>
+        <span class="text-a">A</span>
+        <span class="text-l">L</span>
+      </span>
+    </div>
+  </div>
+  <div class="img-banda">
+    <span>
+    <img  src="assets/images/bandaritas.png">
+    <img  src="assets/images/bandaritas.png">
+  </span>
+  </div>
+
+  <div class="headline">
+					<div class="headline-image text-center">
+						<img src="assets/images/festival.jpg">
+					</div>
+					<div class="headline-item text-left">
+						<div class="headline-title">
+							WHAT IS LOREM IPSUM
+						</div>
+						<div class="row">
+							<span class="col-sm-6 author-name">
+								<i class="fas fa-user-circle"></i> Administrator
+							</span>
+							<span class="col-sm-6 date-posted">
+								<i class="far fa-calendar-times"></i> August 9, 2018
+							</span>
+						</div>
+
+            <img src="assets/images/fireworks.png">
+
+						<div class="headline-content">
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+							tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+							veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+							commodo consequat. Lorem ipsum dolor sit amet, consectetur adipisicing
+							elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+							Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi
+							ut aliquip ex ea commodo consequat bryan is sow pogi i dont know why...
+							<a href="news/what-is-lorem-ipsum" class="read-more" role="button">read
+							more</a>
+						</div>
+					</div>
+				</div>
