d1b1b242247e5c6aa6179b691fb00c92548fe602
tanaytourismthesis <tanaytourismthesis@gmail.com>
2018-10-01 22:44:50 +0800

    Added Hane Finder

--- application/modules/hf/views/hf.php
+++ application/modules/hf/views/hf.php
@@ -1,10 +1,13 @@
-hellow world
-<div class="topnav">
+
+<div class="hane-search">
   <div class="search-container">
     <img src="assets/images/hane.png">
-    <form action="/action_page.php">
-      <input type="text" placeholder="Search.." name="search">
+      <form class="example" action="/action_page.php">
+      <input type="text" placeholder="find a hotel.." name="search">
       <button type="submit"><i class="fa fa-search"></i></button>
     </form>
   </div>
 </div>
+
+ <b>₱ 0</b> <input id="ex2" type="text" class="span2" value="" data-slider-min="0"
+ data-slider-max="1000" data-slider-step="5" data-slider-value="[1000,2000]"/> <b>₱ 10,000</b>
